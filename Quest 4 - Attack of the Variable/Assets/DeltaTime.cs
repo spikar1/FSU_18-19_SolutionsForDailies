ffa57fd08bb1fe7b0c964582df5300471cca67cf
spikar1 <Steffen.fureli@gmail.com>
2018-10-03 16:03:38 +0200

    Created and completed Quest 4

---
+++ Quest 4 - Attack of the Variable/Assets/DeltaTime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeltaTime : MonoBehaviour {
+
+    public float secondsSinceLastFrame;
+    public float timeLastFrame;
+
+    private void Update()
+    {
+        secondsSinceLastFrame = Time.time - timeLastFrame;
+
+        print($"Last frame took {secondsSinceLastFrame} seconds.");
+
+        timeLastFrame = Time.time;
+    }
+
+}
